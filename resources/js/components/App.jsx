ff40c1e7243b8e2e89a902f23fabf1c4a1c94665
69DARKSATAN69 <93655420+69DARKSATAN69@users.noreply.github.com>
2023-03-02 09:24:24 +0100

    a ver si esta vez le da la gana en teoria hay que hacer npm install y composer install despues, en un terminal haces artisan run serve y en otro haces npm run dev

---
+++ resources/js/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Si esto funciona descocha el champan</h1>
+        </div>
+    )
+}
+
+export default App;
