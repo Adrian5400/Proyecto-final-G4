a1342e5551e454757c22358b8fad6d41a40c38e6
Adrian5400 <adrianap57@educastur.es>
2023-03-08 09:08:39 +0100

    Añadida zona pasos y de introducción

---
+++ src/pages/Pasos.jsx
@@ -0,0 +1,30 @@
+import BarraNav from "../../../../06-03/src/pages/BarraNav";
+import React from "react";
+import "./css/pasos.css";
+import Inicio from "./InicioPasos";
+
+function Pasos() {
+    return (
+        <div className="mx-auto contenedor">
+            <div className="container">
+                <div className="row">
+                    <Inicio></Inicio>
+                    </div>
+                </div>
+
+            <footer className="mt-5" style={{ backgroundColor: "var(--color1)", maxWidth: "100vw", overflowX: "hidden" }}>
+                <div className="row">
+                    <div className="col-md-12 text-center">
+                        <p className="mt-3" style={{ color: "var(--color5)" }}>
+                            Lorem ipsum dolor sit amet consectetur Illo!
+                        </p>
+                    </div>
+                </div>
+            </footer>
+        </div>
+    
+
+    );
+}
+
+export default Pasos;
