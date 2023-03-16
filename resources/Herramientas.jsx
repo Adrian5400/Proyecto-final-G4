deab18e7fa11d95bfe332304b3a404e4fc7437ab
Adri <adrialvarezpino@gmail.com>
2023-03-16 18:49:39 +0100

    Herramientas mostradas por la API

--- resources/Herramientas.jsx
+++ resources/Herramientas.jsx
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Herramientas.css';
 
 function Herramientas() {
 
+  const [herramientas, setHerramientas] = useState([]);
   const [detalles, setDetalles] = useState({});
 
   function mostrarDetalles(id, texto) {
+    setDetalles({ herramienta: { id, texto} });
     if (document.querySelector(`#mostrar${id}`).innerHTML == "Ocultar detalles") {
       setDetalles(prevState => ({
         ...prevState,
@@ -24,6 +26,21 @@ function Herramientas() {
   }
   }
 
+  function mostrarApi() {
+
+      const fetchHerramientas = async () => {
+      const response = await fetch('http://127.0.0.1:8000/api/herramientas');
+      const data = await response.json();
+      setHerramientas(data);
+      
+    }
+
+    useEffect(() => {
+      fetchHerramientas();
+  }, []);
+
+  }
+
   return (
     <div>
     <h1 className='pb-3'>Herramientas</h1>
@@ -39,71 +56,22 @@ function Herramientas() {
         <option>Válvula</option>
       </select>
       </div>
+      {mostrarApi()}
       <div className='row offset-2 pb-5 pt-5'>
-      <div id="uno" className="cuerpo card">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostraruno" className="muestra" onClick={() => mostrarDetalles("uno", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
+      {herramientas.map(herramienta => (
+        <div className='row offset-2 pb-5'>
+          <div id={herramienta.id} className="cuerpo card" key={herramienta.id}>
+            <img src={herramienta.image_url} className="imagen card-img-top offset-3"></img>
+            <div className="card-body">
+              <h4 className="card-title">{herramienta.nombre}</h4>
+              <p className="card-text">Pasos en los que se usa: {herramienta.steps}</p>
+              <p id={`mostrar${herramienta.id}`} className="muestra" onClick={() => mostrarDetalles(`${herramienta.id}`, `${herramienta.desc}`)}>Mostrar detalles</p>
+            </div>
+            <div id={`separador${herramienta.id}`}></div>
+            {detalles[herramienta.id] && <h6>{detalles[herramienta.id]}</h6>}
           </div>
-          <div id="separadoruno"></div>
-          {detalles.uno && <h6>{detalles.uno}</h6>}
-        </div>
-        <div id="dos" className="cuerpo card offset-1">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostrardos" className="muestra" onClick={() => mostrarDetalles("dos", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
           </div>
-          <div id="separadordos"></div>
-          {detalles.dos && <h6>{detalles.dos}</h6>}
-        </div>
-    </div>
-    <div className='row offset-2 pb-5'>
-    <div id="tres" className="cuerpo card">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostrartres" className="muestra" onClick={() => mostrarDetalles("tres", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
-          </div>
-          <div id="separadortres"></div>
-          {detalles.tres && <h6>{detalles.tres}</h6>}
-        </div>
-        <div id="cuatro" className="cuerpo card offset-1">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostrarcuatro" className="muestra" onClick={() => mostrarDetalles("cuatro", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
-          </div>
-          <div id="separadorcuatro"></div>
-          {detalles.cuatro && <h6>{detalles.cuatro}</h6>}
-        </div>
-      </div>
-      <div className='row offset-2 pb-5'>
-      <div id="cinco" className="cuerpo card">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostrarcinco" className="muestra" onClick={() => mostrarDetalles("cinco", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
-          </div>
-          <div id="separadorcinco"></div>
-          {detalles.cinco && <h6>{detalles.cinco}</h6>}
-        </div>
-        <div id="seis" className="cuerpo card offset-1">
-          <img src={"https://www.bontempi.es/790-large_default/hoja-de-bisturi-del-15.jpg"} className="imagen card-img-top offset-3"/>
-          <div className="card-body">
-            <h4 className="card-title">{"Bisturi"}</h4>
-            <p className="card-text">{"Se utiliza para hacer cortes limpios y acceder al interior del cuerpo"}</p>
-            <p id="mostrarseis" className="muestra" onClick={() => mostrarDetalles("seis", "Detalles: Hoja fina y afilada hecha de acero, pensada para abrir la piel entre 2 y 5cm de grosor y hasta 50cm de largo")}>Mostrar detalles</p>
-          </div>
-          <div id="separadorseis"></div>
-          {detalles.seis && <h6>{detalles.seis}</h6>}
-        </div>
+        ))}
     </div>
     </div>
     </div>
