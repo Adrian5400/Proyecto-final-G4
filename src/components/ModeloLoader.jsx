c3015c4ac97f7e786900df3f4e8faec66c614f7a
Daniel <danielrg37@educastur.es>
2023-03-18 17:52:41 +0100

    Añadido cambio en HerramientasBusqueda que comente

--- src/components/ModeloLoader.jsx
+++ src/components/ModeloLoader.jsx
@@ -1,12 +1,34 @@
-import { Canvas } from 'react-three-fiber';
-import { useLoader } from 'react-three-fiber';
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
+import "./css/styles.css";
+import { Canvas } from "@react-three/fiber";
+import { Environment, OrbitControls, useFBX } from "@react-three/drei";
+import { Suspense } from "react";
 
-function ModeloLoader({ path }) {
-const fbx = useLoader(FBXLoader, path);
+const Scene = (props) => {
+  const fbx = useFBX(props.url);
 
-  return <primitive object={fbx} />;
+  return <primitive object={fbx} scale={0.005} />;
+};
+
+/* 
+Esto tengo que cambiarle cosas que se ve regulero en las cards, 
+pero funciona. Scene recibe la url como props y la pasa a useFBX, 
+que es el que carga el modelo 3d.  */
+
+export default function ModeloLoader({ url }) {
+  return (
+    <div className="App">
+     <Canvas gl={{ width: 500, height: 500 }}>
+        <Suspense fallback={null}>
+          <Scene url={url} />
+          <OrbitControls />
+          <Environment preset="sunset" background />
+        </Suspense>
+      </Canvas>
+    </div>
+  );
 }
+
+
 
 //PARA FUNCIONAR, DEBE ESTAR METIDO EN UN ELEMENTO <CANVAS></CANVAS>
 //P.EJ <Canvas><ModeloLoader path={path} /></Canvas>
